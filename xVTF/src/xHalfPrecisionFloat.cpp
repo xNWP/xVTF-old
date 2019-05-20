8aa9ea6a59b6f720b80acd385efd51829f65b886
Brett Anthony <nwpbusiness@gmail.com>
2019-05-19 19:18:27 -0600

    Finished half-precision float module.

--- xVTF/src/xHalfPrecisionFloat.cpp
+++ xVTF/src/xHalfPrecisionFloat.cpp
@@ -3,18 +3,20 @@
 float xvtf::half::toFloat() const
 {
 	unsigned char rexp = this->exp + 112;
-	unsigned int man = this->mantissa;
-	unsigned char s = 1;
+	unsigned short int man = this->mantissa;
+	unsigned char s = this->sign;
 
-	char dat[4];
+	unsigned char dat[4];
 	dat[3] = s << 7;
 	dat[3] += ((rexp & 0xFE) >> 1);
-	dat[2] = (rexp & 0x1) << 7;
-	dat[2] += (man & 0xFE000000) >> 25;
-	dat[1] = (man & 0x1FE0000) >> 17;
-	dat[0] = (man & 0x1FE00) >> 9;
+	dat[2] = ((rexp & 0x1) << 7);
+	dat[2] += ((man & 0xFE) >> 1);
+	dat[1] = ((man & 0x1) << 7);
+	dat[1] += ((man & 0xFE00) >> 9);
+	dat[0] = 0;
+	dat[0] += ((man & 0x100) >> 1);
 
-	float test = 0.33333333333333;
+	float out = *(float*)&dat;
 
-	return float();
+	return out;
 }
